d13f2eaccc0bfccc0a7895a9fb6f8f3d423747df
irvincnt <irvincnt93@gmail.com>
2021-03-08 12:30:30 -0600

    Add register component

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import Login from './src/components/auth/Login'
+//import Login from './src/components/auth/Login'
+import Register from './src/components/auth/Register'
 
 export default function App() {
   return (
     <div className='container'>
-      <Login />
+      <Register />
     </div>
   )
 }
