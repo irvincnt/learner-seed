6457055e1638ccd1374bcc5b38abf54118942b3c
irvincnt <irvincnt93@gmail.com>
2021-03-30 19:08:01 -0600

    login dispatch

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,14 +1,31 @@
 import React from "react";
 import "../../styles/login.scss";
 import loginImg from "../../assets/images/login2.svg";
+import { useForm } from "../../hooks/useForm";
+import { useDispatch } from "react-redux";
+import { login } from "../../actions/auth";
 
 export default function Login() {
+  const dispatch = useDispatch();
+
+  const [formValues, handleInputChange] = useForm({
+    email: "irvincnt93@gmail.com",
+    password: "1234567890",
+  });
+
+  const { email, password } = formValues;
+
+  const handlerSubmit = (e) => {
+    e.preventDefault();
+    dispatch(login(12345, "Lucy"));
+  };
+
   return (
     <div className="login">
       <div className="image-login">
         <img src={loginImg} />
       </div>
-      <div className="auth-login">
+      <form onSubmit={handlerSubmit} className="auth-login">
         <div className="auth-login__content">
           <h2 className="title">Bienvenido a EDUCA</h2>
           <p className="subtitle">
@@ -16,17 +33,30 @@ export default function Login() {
           </p>
           <div className="form-group">
             <p className="form-label">Email</p>
-            <input name="name" className="form-control" />
+            <input
+              type="text"
+              name="name"
+              className="form-control"
+              autoComplete="off"
+              value={email}
+              onChange={handleInputChange}
+            />
             <p className="form-message">Requerido</p>
           </div>
           <div className="form-group">
             <p className="form-label">Password</p>
-            <input name="name" className="form-control" />
+            <input
+              type="password"
+              name="name"
+              className="form-control"
+              value={password}
+              onChange={handleInputChange}
+            />
             <p className="form-message">Requerido</p>
           </div>
           <button className="btn">Sing in</button>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
